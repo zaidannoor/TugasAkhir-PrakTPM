828b56e17ba12e20f9c954d365ad2b8f6a4e999d
Zaidan Noor <zaidannoor06@gmail.com>
2023-05-30 23:12:03 +0700

    Commit miraj

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -23,38 +23,82 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black,
         title: Center(child: Text("Profile"),)
         
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 10,
-            ),
-            Image.asset(
-              'images/foto.jpeg',
-              width: 200,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Zaidan Noor Irfan",
-              style: TextStyle(fontSize: 25),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "123200066",
-              style: TextStyle(fontSize: 25),
-            )
-          ],
+      body: Container(
+        color: Color(0xff27374D),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Image.asset(
+                    'images/foto.jpeg',
+                    width: 200,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(width: 200,
+                    child: Text(
+                      "Zaidan Noor Irfan",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "123200066",
+                    style: TextStyle(fontSize: 25),
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Image.asset(
+                    'images/fotor.jpeg',
+                    width: 200,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(width: 200,
+                    child: Text(
+                      "Muhammad Mi'raj Rizky",textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "123200080",
+                    style: TextStyle(fontSize: 25),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Color(0xff27374D),
+         backgroundColor: Colors.black,
         currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
--- lib/pages/regis_page.dart
+++ lib/pages/regis_page.dart
@@ -43,78 +43,127 @@ class _RegisPageState extends State<RegisPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+          backgroundColor: Colors.black,
           title: Center(
-        child: Text('Registration Page'),
-      )),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    controller: usernameController,
-                    decoration: const InputDecoration(
-                      labelText: 'Username',
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your username';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      border: const OutlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your password';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        minimumSize: const Size.fromHeight(50),
-                        textStyle: const TextStyle(fontSize: 20)),
-                    onPressed: _regis,
-                    child: Text('Regis'),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    "Already have an Account ?",
-                    style: TextStyle(
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  SizedBox(height: 15),
-                  InkWell(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginPage()),
-                      );
-                    },
-                    child: Text(
-                      "Login here",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.blue,
-                        decoration: TextDecoration.underline,
+            child: Text('Registration Page'),
+          )),
+      body: Container(
+        color: Color(0xff27374D),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 6,
+                                offset: Offset(0, 2))
+                          ]),
+                      height: 60,
+                      child: TextFormField(
+                        controller: usernameController,
+                        decoration: const InputDecoration(
+                          hintText: 'Username',
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.only(top: 14),
+                          prefixIcon: Icon(
+                            Icons.person,
+                            color: Color(0xff27374D),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your username';
+                          }
+                          return null;
+                        },
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 20),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 6,
+                                offset: Offset(0, 2))
+                          ]),
+                      height: 60,
+                      child: TextFormField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.only(top: 14),
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: Color(0xff27374D),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      width: 300,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.black,
+                            minimumSize: const Size.fromHeight(50),
+                            textStyle: const TextStyle(fontSize: 20),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 15),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15))
+                            ),
+                        onPressed: _regis,
+                        child: Text('Regis'),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "Already have an Account ?",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    InkWell(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginPage()),
+                        );
+                      },
+                      child: Text(
+                        "Login here",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
